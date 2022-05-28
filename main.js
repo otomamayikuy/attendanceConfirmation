252d3e2d8c60117446e9a81322f0bb405a4eaa8c
otomamayikuy <103938277+yukiyamamoto0804@users.noreply.github.com>
2022-05-28 19:08:38 +0900

    init

--- main.js
+++ main.js
@@ -8,17 +8,20 @@ app.use(express.static("static"));
 let menbers = []
 let menbers_number = 0;
 
+//初めての方はこちら
 app.get("/first", (request, response) => {
   const template = fs.readFileSync("static/first.html", "utf-8");
   const html = ejs.render(template);
   response.send(html);
 });
 
+//メンバーの人数を返す
 app.get("/menbers_number", (request, response) => {
   menbers_number = menbers.length + 1;
   response.send(String(menbers_number));
 });
 
+//名前とIDを登録する
 app.get("/register", (request, response) => {
   const name = request.query.name;
   menbers.push({name: name, id: String(menbers_number), statas: "attendance"});
@@ -27,6 +30,7 @@ app.get("/register", (request, response) => {
   response.send(html);
 });
 
+//登録をやり直す
 app.get("/back", (request, response) => {
   menbers_number--;
   menbers.pop();
@@ -35,30 +39,34 @@ app.get("/back", (request, response) => {
   response.send(html);
 });
 
+//ログインして始める
 app.get("/start", (request, response) => {
   const name = request.query.name;
   const number = String(request.query.number);
   let count = 0;
   for(let i = 0; i < menbers.length; i++){
     if(menbers[i].name === name && menbers[i].id === number){
+      menbers[i].statas = "attendance";
       const template = fs.readFileSync("static/attendance.html", "utf-8");
-      const html = ejs.render(template);
+      const html = ejs.render(template, {name: name, id: number});
       response.send(html);
     }else{
       count++
     }
   }
   if(count === menbers.length){
-    const template = fs.readFileSync("static/index.html", "utf-8");
+    const template = fs.readFileSync("static/home.html", "utf-8");
     const html = ejs.render(template);
     response.send(html);
   }
 });
 
+//メンバーの情報を返す
 app.get("/menbers", (request, response) => {
   response.json(menbers);
 });
 
+//退出する
 app.get("/exit", (request, response) => {
   const name = request.query.name;
   const number = String(request.query.number);
@@ -70,6 +78,7 @@ app.get("/exit", (request, response) => {
   response.send(menbers);
 });
 
+//出席にする
 app.get("/attend", (request, response) => {
   const name = request.query.name;
   const number = String(request.query.number);
@@ -80,5 +89,33 @@ app.get("/attend", (request, response) => {
   }
   response.send(menbers);
 });
+
+//ログアウトする
+app.get("/home", (request,response) => {
+  const template = fs.readFileSync("static/home.html", "utf-8");
+  const html = ejs.render(template);
+  response.send(html);
+});
+
+//編集する
+app.get("/edit_name", (request, response) => {
+  const new_name = request.query.new_name;
+  const name = request.query.name;
+  const number = String(request.query.number);
+  for(let i = 0; i < menbers.length; i++){
+    if(menbers[i].name === name && menbers[i].id === number){
+      menbers[i].name = new_name;
+    }
+  }
+  response.send(number)
+});
+
+//保存する
+app.get("/save", (request, response) => {
+  const name = request.query.name;
+  const number = String(request.query.number);
+  response.json({name: name, id: number});
+});
+
 
 app.listen(3000);
--- static/attendance.js
+++ static/attendance.js
@@ -1,16 +1,32 @@
+const ul = document.getElementById("attend_list")
+let lis = [];
 setInterval(get_menbers = async () => {
     const response = await fetch("/menbers");
     const text = await response.text();
     const menbers = JSON.parse(text);
-    let attend_menbers = "";
+    let attend_menbers = [];
+    for(const li of lis){
+        li.remove()
+    }
+    lis.length = 0;
     for(const menber of menbers){
         if(menber.statas === "attendance"){
-            attend_menbers += `<li>${menber.name}</li>`;
+            attend_menbers.push(menber);
         }
     }
-    document.getElementById("attend_list").innerHTML = attend_menbers;
-}, 1000);
+    for(const attend_menber of attend_menbers){
+        lis.push(document.createElement("li"));
+        lis[lis.length-1].innerText = attend_menber.name
+        ul.appendChild(lis[lis.length-1]);
+    }
+}, 200);
 
+const save = async () => {
+    const param = document.location.search;
+    const response = await fetch("/save"+param);
+    const text = await response.text();
+    localStorage.setItem("info",text);
+}
 const exit = async () => {
     const param = document.location.search;
     const response = await fetch("/exit"+param);
@@ -20,3 +36,41 @@ const attend = async () => {
     const param = document.location.search;
     const response = await fetch("/attend"+param);
 }
+
+const logout = async () => {
+    const param = document.location.search;
+    const response = await fetch("/exit"+param);
+    alert("ログアウトしました");
+    location = "/home";
+}
+
+function edit() {
+    const edit_text = document.getElementById("edit_text");
+    const name_box = document.createElement("input");
+    name_box.id = "name_box";
+    const name_button = document.createElement("button");
+    name_button.innerText = "登録";
+    edit_text.innerText = "変更後の名前を入力してください";
+    edit_text.appendChild(name_box);
+    edit_text.appendChild(name_button);
+    name_button.onclick = edit_name;
+}
+
+const edit_name = async () => {
+    const param = document.location.search;
+    const name_box = document.getElementById("name_box")
+    const new_name = name_box.value;
+    const response = await fetch(`/edit_name${param}&new_name=${new_name}`);
+    const number = await response.text();
+    location = `/start?number=${number}&name=${new_name}`;
+}
+
+save()
+const exit_button = document.getElementById("exit");
+exit_button.onclick = exit;
+const attend_button = document.getElementById("attend");
+attend_button.onclick = attend;
+const logout_button = document.getElementById("logout");
+logout_button.onclick = logout;
+const edit_button = document.getElementById("edit");
+edit_button.onclick = edit;
